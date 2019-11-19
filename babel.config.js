83a376b1ba1fc5ca9cc0e152ffae287e1da5fb66
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-19 09:03:10 -0300

    adiciona eslint + editoconfig + prettier + module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
+const plugins = [
+    [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+            root: ['./src/'],
+        },
+    ],
+];
+
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins,
 };
